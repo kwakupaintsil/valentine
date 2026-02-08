1012f841a854f1e112629cf057321970e1127b26
Kwaku Paintsil <paintsilbuabeng26@gmail.com>
2026-02-08 12:46:15 +1100

    Song update

--- app/page.tsx
+++ app/page.tsx
@@ -318,7 +318,7 @@ export default function ValentinePage() {
               <iframe
                 width="100%"
                 height="200"
-                src="https://www.youtube.com/embed/JxW9qXlWlpo"
+                src="https://www.youtube.com/embed/Crph6eCkaow"
                 title="First Time by Teeks"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
